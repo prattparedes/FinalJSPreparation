fefde40e3f433c35c2e3030864870f093a4e19f1
Pratt <pratt.paredes@gmail.com>
2022-09-26 20:30:04 -0500

    final preparations done

--- user.js
+++ user.js
@@ -1,20 +1,14 @@
 const postListEl = document.querySelector(' .post-list')
+const id = localStorage.getItem("id")
 
 async function onSearchChange(event) {
     const id = event.target.value
-    const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
-    const postsData = await posts.json()
-    console.log(postsData)
-
-    postListEl.innerHTML = postsData.map(post => postHTML(post)).join('')
+    renderPosts(id)
 }
 
-async function main() {
-    const id = localStorage.getItem("id")
-    const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
+async function renderPosts(userId) {
+    const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)
     const postsData = await posts.json()
-    console.log(postsData)
-
     postListEl.innerHTML = postsData.map(post => postHTML(post)).join('')
 }
 
@@ -30,4 +24,4 @@ function postHTML(post) {
     </div>`
 }
 
-main()
+renderPosts(id)
